f6aab32727b1d33ebba8ef6ae5a07fb77fd43d5b
Volodymyr Yevtushenko <voloyev@vivali.net>
2018-04-04 13:36:33 +0300

    Add func for create deck

--- main.go
+++ main.go
@@ -2,11 +2,6 @@ package main
 
 func main() {
 	//	var card string = "Ace of Spades"
-	cards := deck{"Ace of Spades", newCard()}
-	cards = append(cards, "Five of Diamonds")
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
