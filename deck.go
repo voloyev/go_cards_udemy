02b148ce0ea3d4daf8c74697ea15b27a59a2fe0f
Volodymyr Yevtushenko <voloyev@vivali.net>
2018-04-12 17:13:10 +0300

    Add saveToFile functon

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+)
 
 type deck []string
 
@@ -25,4 +29,12 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",") 
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
+import "fmt"
 
 func main() {
 	//	var card string = "Ace of Spades"
 	cards := newDeck()
-
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	remainingDeck.print()
+	fmt.Println(cards.toString())
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.print()
+	// remainingDeck.print()
+	cards.saveToFile("test")
 }
