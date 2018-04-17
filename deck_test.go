04f44bba5d0062c6860d363bc8d9ce8ed33ff627
Volodymyr Yevtushenko <voloyev@vivali.net>
2018-04-17 13:44:58 +0300

    Update test

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -17,4 +20,17 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected last Four of Clubs but got %v", d[len(d)-1])
 	}
+}
+
+func TestSavetoDeckAndNewFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
